2feee076028af8dfa09cd3e55ec1233517f2523a
Tichakorn <tichakorn06@hotmail.com>
2014-02-19 15:27:03 +0700

    02-19-2014 krain theme

--- krain-theme-02-19-2014/bootstrap/js/bootstrap.js
+++ krain-theme-02-19-2014/bootstrap/js/bootstrap.js
@@ -237,7 +237,7 @@
   $(function () {
     $('body').on('click.button.data-api', '[data-toggle^=button]', function ( e ) {
       var $btn = $(e.target)
-      if (!$btn.hasClass('btn')) $btn = $btn.closest('.btn')
+      if (!$btn.hasClass('.btn')) $btn = $btn.closest('.btn')
       $btn.button('toggle')
     })
   })
